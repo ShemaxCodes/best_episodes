5cea5f042073b24662995ff248007d50c86be000
Jashema Panapa <jcpanapa@gmail.com>
2020-05-03 12:54:02 -0400

    added specs

--- lib/best_episodes/scraper.rb
+++ lib/best_episodes/scraper.rb
@@ -1,12 +1,19 @@
 class BestEpisodes::Scraper 
 
-@@all = []
+@@episodes = []
 
 def initialize
 
-
+html = open("https://www.cleveland.com/entertainment/2020/03/the-office-at-15-ranking-the-10-best-episodes.html")
+doc = 
 
 end 
+
+def self.scrape_episodes 
+    
+
+end 
+
 
 
 end 
