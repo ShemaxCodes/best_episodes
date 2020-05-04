01c9e56f1e0f945c4b2652ad9a33b2cc6545f9c6
Jashema Panapa <jcpanapa@gmail.com>
2020-05-04 13:01:10 -0400

    New selector for episode title, season, and episode number

--- lib/best_episodes/episode.rb
+++ lib/best_episodes/episode.rb
@@ -6,20 +6,16 @@ require 'pry'
 
     class Episode 
         attr_accessor :title, :season, :episode_number 
-
-
         @@episodes = []
-
         def self.get_page
-        html = open("https://www.cleveland.com/entertainment/2020/03/the-office-at-15-ranking-the-10-best-episodes.html")
-        doc = Nokogiri::HTML(html)
+
+            html = open("https://www.cleveland.com/entertainment/2020/03/the-office-at-15-ranking-the-10-best-episodes.html")
+            doc = Nokogiri::HTML(html)
         end 
 
         def self.scrape_show 
             binding.pry
-            get_page.css("b").text
-            @@episodes << get_page.css("b").text
-            @@episodes
+            get_page.css("#YWH273QDXVBTLBLL4ALSVSNUGU").text
             
         end 
 
