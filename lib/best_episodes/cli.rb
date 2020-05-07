b3187ac3e760e432403c6c9d4c40182c1c5915bf
Jashema Panapa <jcpanapa@gmail.com>
2020-05-06 23:49:50 -0400

    added to cli

--- lib/best_episodes/cli.rb
+++ lib/best_episodes/cli.rb
@@ -4,28 +4,40 @@
 class CLI
 
    def call 
-
+      puts " "
       puts "Welcome to Best Episodes for The Office Super Fans !"
       puts "====================================================" 
+      puts " "
+      puts "Please select the show by its number."
+      puts "1. The Office"
+      puts " "
+      @number = gets.chomp 
 
+      if @number == 1 
+         puts "**Best Episodes for The Office**"
+         puts "================================="
+         puts ep_ten 
+         puts get_page.css("p.article__paragraph.article__paragraph--left b")[1].text
+         puts get_page.css("p.article__paragraph.article__paragraph--left b")[2].text
+         puts get_page.css("p.article__paragraph.article__paragraph--left b")[3].text
+         puts get_page.css("p.article__paragraph.article__paragraph--left b")[4].text
+      else 
+         puts "Please try again."
    end 
 
 
-   def list_show 
-      puts "The Office"
-   end 
+   
 #Asks user if they want to continue (y/n)
-   def menu 
-      puts "Enter 'Y' for Yes - 'N' for No."
-   end 
+  
 
    def list_episodes
+      ep_ten = get_page.css("#YWH273QDXVBTLBLL4ALSVSNUGU").text
 
    end 
 
 
    def description
-      puts
+      ten_des = get_page.css("#2H5WFESI2NFFHEFCZKABI7JKTU").text
    end 
 
 
